35ee8ae92a3c4e4976e57814be6902e1a38010d1
Abubakar <abubakkeramjad10@gmail.com>
2024-09-14 14:17:12 +0500

    server actions setup

---
+++ server-actions/src/app/theory/server-page-example/page.js
@@ -0,0 +1,10 @@
+// server actions are asynchronous functions that are exectued on the server 
+
+function ServerActions(){
+   return(
+    <div>
+        <h1>Server actions example - server components</h1>
+    </div>
+   )
+}
+export default ServerActions
